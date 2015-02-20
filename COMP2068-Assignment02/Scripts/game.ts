6831a4923dceee72a7221d6646f372d8f3bd0f77
karan sharma <karan.sharma516@yahoo.com>
2015-02-20 00:18:01 -0800

    added all the functions as per requirement and added spinReel function which shows the images and text

--- COMP2068-Assignment02/Scripts/game.ts
+++ COMP2068-Assignment02/Scripts/game.ts
@@ -171,7 +171,7 @@ function createUI() {
     powerButton.y = 122;
 
     // power Button Event Listeners
-    powerButton.addEventListener("click", );
+    powerButton.addEventListener("click", closeWindow );
     powerButton.addEventListener("mouseover", powerButtonOver);
     powerButton.addEventListener("mouseout", powerButtonOut);
 
@@ -208,4 +208,271 @@ function createUI() {
     betTenButton.addEventListener("click", betTen);
     betTenButton.addEventListener("mouseover", betTenButtonOver);
     betTenButton.addEventListener("mouseout", betTenButtonOut);
+
+    // instantiate the player credits
+    playerCreditsText = new createjs.Text("" + playerMoney, "20px Arial", "#ff7700");
+    playerCreditsText.x = 102;
+    playerCreditsText.y = 352;
+    game.addChild(playerCreditsText);
+
+    // instantiate the bet amount
+    betText = new createjs.Text(playerBet.toString() , "20px Arial", "#ff7700");
+    betText.x = 223;
+    betText.y = 352;
+    game.addChild(betText);
+
+    // instantiate the winning amount
+    winningsText = new createjs.Text(winnings.toString() , "20px Arial", "#ff7700");
+    winningsText.x = 308;
+    winningsText.y = 352;
+    game.addChild(winningsText);
+
+    // instantiate the jackpot
+    jackpotText = new createjs.Text( jackpot.toString() , "20px Arial", "#ff7700");
+    jackpotText.x = 235;
+    jackpotText.y = 133;
+    game.addChild(jackpotText);
+}
+
+function spinReels() {
+
+    if (playerMoney == 0) {
+        if (confirm("You ran out of Money! \nDo you want to play again?")) {
+            resetAll();
+            showPlayerStats();
+        }
+    }
+    else if (playerBet > playerMoney) {
+        alert("You don't have enough Money to place that bet.");
+    }
+
+    else if (playerBet <= playerMoney) {
+
+        // Add Spin Reels code here
+        spinResult = Reels();
+        fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+        console.log(fruits);
+
+        for (var tile = 0; tile < 3; tile++) {
+
+            tileContainers[tile].removeAllChildren();
+            tiles[tile] = new createjs.Bitmap("assets/images/" + spinResult[tile] + ".png");
+            tiles[tile].x = 110 + (113 * tile);
+            tiles[tile].y = 239;
+            tileContainers[tile].addChild(tiles[tile]);
+        }
+        determineWinnings();
+        showPlayerStats();
+    }
+}
+
+// this method displays the bet amount of $1 when user clicks the betMin button
+function betOne() {
+    playerBet = 1;
+    betText.text = playerBet.toString();
+} //function bet1 ends 
+
+// this method displays the bet amount of $100 when user clicks the betMax button
+function betMax() {
+    playerBet = 100;
+    betText.text = playerBet.toString();
+
+} //function bet100 ends 
+
+// this method displays the bet amount of $10 when user clicks the bet10 button
+function betTen() {
+    playerBet = 10;
+    betText.text = playerBet.toString();
+
+} //function bet1 ends 
+
+function closeWindow() {
+    var x = confirm('Are You sure want to exit:');
+    if (x) window.close();
+}
+
+/* Utility function to show Player Stats */
+function showPlayerStats() {
+    winRatio = winNumber / turn;
+  //  jackpotText.text = "Jackpot: " + jackpot; 
+    playerCreditsText.text = playerMoney.toString();
+} 
+
+/* Check to see if the player won the jackpot */
+function checkJackPot() {
+    /* compare two random values */
+    var jackPotTry = Math.floor(Math.random() * 51 + 1);
+    var jackPotWin = Math.floor(Math.random() * 51 + 1);
+    if (jackPotTry == jackPotWin) {
+        alert("You Won the $" + jackpot + " Jackpot!!");
+        playerMoney += jackpot;
+        jackpot = 1000;
+    }
+}
+
+/* Utility function to show a win message and increase player money */
+function showWinMessage() {
+    playerMoney += winnings;
+    winningsText.text = winnings.toString();
+    resetFruitTally();
+    checkJackPot();
+}
+
+/* Utility function to show a loss message and reduce player money */
+function showLossMessage() {
+    playerMoney -= playerBet;
+    winningsText.text = "0";
+    resetFruitTally();
+    // playerCreditsText.text = "" + playerMoney;
+}
+
+/* Utility function to check if a value falls within a range of bounds */
+function checkRange(value, lowerBounds, upperBounds) {
+    if (value >= lowerBounds && value <= upperBounds) {
+        return value;
+    }
+    else {
+        return !value;
+    }
+}
+
+
+/* Utility function to reset all fruit tallies */
+function resetFruitTally() {
+    grapes = 0;
+    bananas = 0;
+    oranges = 0;
+    cherries = 0;
+    bar = 0;
+    bells = 0;
+    sevens = 0;
+    blanks = 0;
+}
+
+/* Utility function to reset the player stats */
+function resetAll() {
+    for (var i = 0; i < 3; i++) {
+        tileContainers[i].removeAllChildren();
+    }
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+    betText.text = playerBet.toString();
+    playerCreditsText.text = playerMoney.toString();
+    winningsText.text = winnings.toString();
+
+} 
+
+/* When this function is called it determines the betLine results.
+e.g. Bar - Orange - Banana */
+function Reels() {
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+
+    for (var spin = 0; spin < 3; spin++) {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin]) {
+            case checkRange(outCome[spin], 1, 27):  // 41.5% probability
+                betLine[spin] = "blank";
+                blanks++;
+                break;
+            case checkRange(outCome[spin], 28, 37): // 15.4% probability
+                betLine[spin] = "grapes";
+                grapes++;
+                break;
+            case checkRange(outCome[spin], 38, 46): // 13.8% probability
+                betLine[spin] = "banana";
+                bananas++;
+                break;
+            case checkRange(outCome[spin], 47, 54): // 12.3% probability
+                betLine[spin] = "orange";
+                oranges++;
+                break;
+            case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                betLine[spin] = "cherry";
+                cherries++;
+                break;
+            case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                betLine[spin] = "bar";
+                bar++;
+                break;
+            case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                betLine[spin] = "bell";
+                bells++;
+                break;
+            case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                betLine[spin] = "seven";
+                sevens++;
+                break;
+        }
+    }
+    return betLine;
+}
+
+
+/* This function calculates the player's winnings, if any */
+function determineWinnings() {
+    if (blanks == 0) {
+        if (grapes == 3) {
+            winnings = playerBet * 10;
+        }
+        else if (bananas == 3) {
+            winnings = playerBet * 20;
+        }
+        else if (oranges == 3) {
+            winnings = playerBet * 30;
+        }
+        else if (cherries == 3) {
+            winnings = playerBet * 40;
+        }
+        else if (bar == 3) {
+            winnings = playerBet * 50;
+        }
+        else if (bells == 3) {
+            winnings = playerBet * 75;
+        }
+        else if (sevens == 3) {
+            winnings = playerBet * 100;
+        }
+        else if (grapes == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (bananas == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (oranges == 2) {
+            winnings = playerBet * 3;
+        }
+        else if (cherries == 2) {
+            winnings = playerBet * 4;
+        }
+        else if (bar == 2) {
+            winnings = playerBet * 5;
+        }
+        else if (bells == 2) {
+            winnings = playerBet * 10;
+        }
+        else if (sevens == 2) {
+            winnings = playerBet * 20;
+        }
+        else {
+            winnings = playerBet * 1;
+        }
+
+        if (sevens == 1) {
+            winnings = playerBet * 5;
+        }
+        winNumber++;
+        showWinMessage();
+    }
+    else {
+        lossNumber++;
+        showLossMessage();
+    }
+
 }
